9188a53a96d0fb248a81c3a137630451c70aedde
Peter Naydenov <peter.naydenov@gmail.com>
2026-07-10 19:43:57 +0300

    code:  Fixes v.1.0.3

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,12 @@ export default {
       file: 'dist/url-pattern.cjs.js',
       format: 'cjs',
       name: 'urlPattern',
-      exports: 'named'
+      exports: 'named',
+      // Make `require('@peter.naydenov/url-pattern')` return the factory
+      // function directly so the README's `const urlPattern = require(...)`
+      // example works. The named exports remain accessible as properties of
+      // that function.
+      outro: 'module.exports = Object.assign(module.exports.default, module.exports);'
     },
     {
       file: 'dist/url-pattern.umd.js',
